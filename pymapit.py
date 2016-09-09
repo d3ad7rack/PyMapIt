f4722eea2ab6f0164d486e7d449fe87e6a155db4
d3ad7rack <william.o.frye@gmail.com>
2016-09-09 14:05:08 -0400

    Took out some things on the ToDo list, and started adding a vulnerability check menu for later usage.

--- pymapit.py
+++ pymapit.py
@@ -9,35 +9,26 @@ __doc__ = """
 PyMap-IT http://www.security-focused.com
 
 #TODO:
+# Classes:
+#  Class for vulnerability checking
+#
 # Scan Types:
-#  -e <interface> (Use specified interface) <- probably require another menu
-#  --source-port <portnumber>; -g <portnumber> (Spoof source port number)
-#  --data <hex string> (Append custom binary data to sent packets)
-#  --data-string <string> (Append custom string to sent packets)
-#  --data-length <number> (Append random data to sent packets)
-#  --ttl <value> (Set IP time-to-live field)
 #  --randomize-hosts (Randomize target host order)
-#  --spoof-mac <MAC address, prefix, or vendor name> (Spoof MAC address)
+#  --spoof-mac <MAC> address, prefix, or vendor name> (Spoof MAC address)
 #  --proxies <Comma-separated list of proxy URLs> (Relay TCP connections through a chain of proxies)
 #  --badsum (Send packets with bogus TCP/UDP checksums)
 #  --adler32 (Use deprecated Adler32 instead of CRC32C for SCTP checksums)
 #
-# Functions:
-#  Exit()
-#
 # Done:
 #  -D <decoy1>[,<decoy2>][,ME][,...] (Cloak a scan with decoys)
 #  -S <IP_Address> (Spoof source address)
+#  Working exit function
 
 
   by Will Frye, d3ad7rack
 """
 
-
-# Guts of the program, for selected scan types
-# def exit_out():
-
-
+#class vuln_check:
 
 class evade_fw:
 	def decoy_address(self):
@@ -104,12 +95,28 @@ def xsl_file():
 	print("")
 	raw_input("Press any key to continue...")
 
+def no_options():
+	os.system('clear')
+	print('What IP, IP-range (CIDR or range of xxx-xxx i.e. 192.168.1.1-254, or domain would you like to scan? ')
+	ip = raw_input('IP, CIDR, Range, or Domain: ')
+	print("Now going to run nmap scan with no options, saving output to no-options-" + ip + ".nmap and no-options-" + ip + ".xml for later parsing.")
+	os.system('sudo nmap --stylesheet=nmap.xsl -oX no-options-' + ip + '.xml -oN no-options-' + ip + '.nmap ' + ip)
+	os.system('clear')
+	print("Scan ran against " + ip + " and saved to current directory under no-options-" + ip + ".nmap and no-options-" + ip + ".xml for your parsing pleasure!")
+	print("")
+	print("Scan against " + ip + " ran, now giving you the ports that were open and their services running on them...")
+	print("Open ports tcp/udp with software versions are...")
+	print("")
+	os.system('cat no-options-' + ip + '.nmap | grep open | grep tcp||udp')
+	print("")
+	print("Thank you for using d3ad7rack's fast and easy nmap scan-selector python program!!")
+	print("")
+	raw_input("Press any key to continue...")
+
 def basic_loud_scan():
 	os.system('clear')
 	print('What IP, IP-range (CIDR or range of xxx-xxx i.e. 192.168.1.1-254, or domain would you like to scan? ')
 	ip = raw_input('IP, CIDR, Range, or Domain: ')
-	print('What ports are you wanting to scan against? Can be entered as xxx-yyy, x,y,z, or a single port')
-	ports = raw_input('Ports: ')
 	print("Now going to run nmap scan with -Pn -sV -A -T4 options, saving output to loud-scan-" + ip + ".nmap and loud-scan-" + ip + ".xml for later parsing.")
 	os.system('sudo nmap -Pn -sV -A -T4 --stylesheet=nmap.xsl -oX loud-scan-' + ip + '.xml -oN loud-scan-' + ip + '.nmap ' + ip)
 	os.system('clear')
@@ -129,12 +136,16 @@ def exit():
 
 
 evade = evade_fw()
+#safe = safe_vuln_check()
+#unsafe = unsafe_vuln_check()
 
 # Main program running dialogue
 def main():  # menu goes here
 	opt_list = [xsl_file,
+				no_options,
 				basic_loud_scan,
 				fw_main,
+				vuln_main,
 				exit
 				]
 
@@ -142,9 +153,11 @@ def main():  # menu goes here
 		os.system('clear')
 		print("SELECT OPTION: ")
 		print("1\tCopy nmap.xsl file to present directory for proper saving of xml files (Needs to be done)")
-		print("2\tBasic, Loud Scan")
-		print("3\tEvade Firewalls")
-		print("4\tExit")
+		print("2\tBasic Scan, No Options")
+		print("3\tBasic, Loud Scan")
+		print("4\tEvade Firewalls")
+		print("5\tVulnerability Checking")
+		print("6\tExit")
 		opt_choice = int(raw_input("Selection: "))
 		os.system('clear')
 		opt_choice -= 1
@@ -174,6 +187,67 @@ def fw_main():  # menu goes here
 		opt_list[opt_choice]()
 
 	return
+
+def vuln_main():  # menu goes here
+	opt_list = [safe_vulns_main,
+				unsafe_vulns_main,
+				main
+				]
+
+	while (True):
+		os.system('clear')
+		print("SELECT OPTION: ")
+		print("1\tSafe Vulnerability Checking")
+		print("2\tUnsafe Vulnerability Checking")
+		print("3\tMain Menu")
+		opt_choice = int(raw_input("Selection: "))
+		os.system('clear')
+		opt_choice -= 1
+		opt_list[opt_choice]()
+
+	return
+
+#def safe_vulns_main():  # menu goes here
+#	opt_list = [safe.http,
+#				safe.ssh,
+#				main
+#				]
+#
+#	while (True):
+#		os.system('clear')
+#		print("SELECT OPTION: ")
+#		print("1\tHTTP(S) Vulnerabilties")
+#		print("2\tSSH Vulnerabilities")
+#		print("3\t")
+#		print("4\t")
+#		print("5\t")
+#		opt_choice = int(raw_input("Selection: "))
+#		os.system('clear')
+#		opt_choice -= 1
+#		opt_list[opt_choice]()
+#
+#	return
+
+#def unsafe_vulns_main():  # menu goes here
+#	opt_list = [unsafe.http,
+#				unsafe.ssh,
+#				main
+#				]
+#
+#	while (True):
+#		os.system('clear')
+#		print("SELECT OPTION: ")
+#		print("1\tHTTP(S) Vulnerabilties")
+#		print("2\tSSH Vulnerabilties")
+#		print("3\t")
+#		print("4\t")
+#		print("5\t")
+#		opt_choice = int(raw_input("Selection: "))
+#		os.system('clear')
+#		opt_choice -= 1
+#		opt_list[opt_choice]()
+#
+#	return
 
 
 main()
